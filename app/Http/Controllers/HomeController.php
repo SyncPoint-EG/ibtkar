6e7df7e3892885baa91fd57b779d421f65b81e6f
Yassin3007 <yassin.ahmed3070@gmail.com>
2025-08-03 17:00:22 +0300

    some fixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CentersResource;
+use App\Http\Resources\DivisionsResource;
+use App\Http\Resources\GovernoratesResource;
+use App\Http\Resources\GradesResource;
+use App\Http\Resources\StagesResource;
+use App\Models\Center;
+use App\Models\District;
+use App\Models\Division;
+use App\Models\Governorate;
+use App\Models\Grade;
+use App\Models\Stage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Session;
@@ -22,4 +33,40 @@ class HomeController extends Controller
         // Redirect back to the previous page
         return redirect()->back();
     }
+
+    public function getStages(){
+        $stages = Grade::all();
+        return StagesResource::collection($stages);
+    }
+
+    public function getGrades($id)
+    {
+        $grades = Grade::where('stage_id', $id)->get();
+        return GradesResource::collection($grades);
+    }
+    public function getDivisions(Stage $stage, Grade $grade)
+    {
+        $divisions = Division::where('stage_id', $stage->id)
+            ->where('grade_id', $grade->id)
+            ->get();
+        return DivisionsResource::collection($divisions);
+    }
+
+    public function getCenters()
+    {
+        $centers = Center::all();
+        return CentersResource::collection($centers);
+    }
+
+    public function getGovernorates()
+    {
+        $governorates = Governorate::all();
+        return GovernoratesResource::collection($governorates);
+    }
+    public function getDistricts(Governorate $governorate)
+    {
+        $districts = District::where('governorate_id', $governorate->id)->get();
+        return response()->json($districts);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('stages',[HomeController::class, 'getStages']);
+Route::get('grades/{id}',[HomeController::class, 'getGrades']);
+Route::get('divisions/{stage}/{grade}',[HomeController::class, 'getDivisions']);
+Route::get('centers',[HomeController::class, 'getCenters']);
+Route::get('governorates',[HomeController::class, 'getGovernorates']);
+Route::get('districts/{governorate}',[HomeController::class, 'getDistricts']);
