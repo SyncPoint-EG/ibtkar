1b5c9cd8380f095045506d77717c345d652392cf
Yassin3007 <yassin.ahmed3070@gmail.com>
2025-08-20 17:40:20 +0300

    some edits

--- app/Http/Resources/LessonResource.php
+++ app/Http/Resources/LessonResource.php
@@ -14,6 +14,20 @@ class LessonResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'desc' => $this->desc,
+            'video_link' => $this->video_link,
+            'video_image' => $this->video_image,
+            'is_featured' => $this->is_featured,
+            'type' => $this->type,
+            'chapter_id' => $this->chapter_id,
+            'course_id' => $this->chapter ? $this->chapter->course_id : null,
+            'subject' => new SubjectResource($this->chapter->course->subject),
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+
+        ];
     }
 }
