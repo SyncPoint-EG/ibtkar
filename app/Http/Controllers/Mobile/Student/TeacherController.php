e851e589f4aed35e5191fa345d5c0051fbb3e52a
Yassin3007 <yassin.ahmed3070@gmail.com>
2025-08-27 12:12:58 +0300

    some updates

--- app/Http/Controllers/Mobile/Student/TeacherController.php
+++ app/Http/Controllers/Mobile/Student/TeacherController.php
@@ -90,14 +90,14 @@ class TeacherController extends Controller
         $lessons = Lesson::whereHas('chapter.course', function ($q) use ($teacher) {
 //            $q->where('teacher_id', $teacher->id);
         })
-//            ->whereDate('date', '>=', now())
-//          ->with(['chapter.course.subject'])
+            ->whereDate('date', '>=', now())
+          ->with(['chapter.course.subject'])
           ->get();
 
 
-//        $lessonsBySubject = $lessons->groupBy(function ($lesson) {
-//            return $lesson->chapter->course->subject->name;
-//        });
+        $lessonsBySubject = $lessons->groupBy(function ($lesson) {
+            return $lesson->chapter->course->subject->name;
+        });
 
         return LessonResource::collection($lessons);
     }
