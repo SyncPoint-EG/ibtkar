f0350c4e537f4d97b5bbc3aa30b57c82406e0ca1
Yassin3007 <yassin.ahmed3070@gmail.com>
2025-08-29 15:05:28 +0300

    some updates

--- app/Http/Controllers/Mobile/Student/FavoritesController.php
+++ app/Http/Controllers/Mobile/Student/FavoritesController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers\Mobile\Student;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\LessonResource;
 use Illuminate\Http\Request;
 
 class FavoritesController extends Controller
 {
+    public function listFavorites()
+    {
+        $student = auth('student')->user();
+        return LessonResource::collection($student->favorites);
+    }
     public function addToFavorite(Request $request)
     {
         $request->validate([
--- routes/students.php
+++ routes/students.php
@@ -88,5 +88,6 @@ Route::group(['middleware' => 'auth:student'], function () {
 
     // favorites routes
 
+    Route::get('favorite',[FavoritesController::class,'listFavorites']);
     Route::post('add-to-favorite',[FavoritesController::class,'addToFavorite']);
 });
