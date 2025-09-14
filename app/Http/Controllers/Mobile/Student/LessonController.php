527a9919868fc3177cb106359accd7c3bd88f14f
Yassin3007 <yassin.ahmed3070@gmail.com>
2025-09-14 22:50:43 +0300

    some

--- app/Http/Controllers/Mobile/Student/LessonController.php
+++ app/Http/Controllers/Mobile/Student/LessonController.php
@@ -15,7 +15,7 @@ class LessonController extends Controller
     {
         $perPage = \request()->query('perPage',10);
         $student = auth('student')->user();
-        return Lesson::with(['chapter:id','chapter.course:id'])->get() ;
+        return Lesson::with(['chapter:id','chapter.course'])->get() ;
         $lessons = Lesson::where('is_featured',1)->whereHas('chapter.course',function ($q) use ($student){
             $q->where('stage_id',$student->stage_id);
                 $q->where('grade_id',$student->grade_id);
