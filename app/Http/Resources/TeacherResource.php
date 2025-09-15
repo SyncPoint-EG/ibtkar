6fba6f777e16eed4d6c83bca41af266b0b25ec34
Yassin3007 <yassin.ahmed3070@gmail.com>
2025-09-15 23:59:22 +0300

    .

--- app/Http/Resources/TeacherResource.php
+++ app/Http/Resources/TeacherResource.php
@@ -18,8 +18,14 @@ class TeacherResource extends JsonResource
         $coursesQuery = $this->courses();
         if ($student) {
             $coursesQuery->where('stage_id', $student->stage_id)
-                ->where('grade_id', $student->grade_id)
-                ->where('division_id', $student->division_id);
+                ->where('grade_id', $student->grade_id);
+
+            if ($student->division_id) {
+                $coursesQuery->where(function ($query) use ($student) {
+                    $query->where('division_id', $student->division_id)
+                        ->orWhereNull('division_id');
+                });
+            }
         }
         $courses = $coursesQuery->get()->unique('id');
         $courseIds = $courses->pluck('id');
