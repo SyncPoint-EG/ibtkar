474ea1047bef9dfcd761757abaa8932b786addac
Yassin3007 <yassin.ahmed3070@gmail.com>
2025-09-23 02:57:32 +0300

    ss

--- resources/views/dashboard/layouts/master.blade.php
+++ resources/views/dashboard/layouts/master.blade.php
@@ -73,22 +73,22 @@
     <style>
     /* Desktop styles for centered logo */
     @media (min-width: 768px) {
-        .navbar-header {
-            position: relative;
-        }
-        .logo-container {
-            position: absolute;
-            left: 50%;
-            transform: translateX(-50%);
-            top: 0;
-        }
+        /*.navbar-header {*/
+        /*    position: relative;*/
+        /*}*/
+        /*.logo-container {*/
+        /*    position: absolute;*/
+        /*    left: 50%;*/
+        /*    transform: translateX(-50%);*/
+        /*    top: 0;*/
+        /*}*/
     }
 
     /* Shared styles for logo size */
-    .logo-container .brand-logo {
-        height: 90px;
-        width: auto;
-    }
+    /*.logo-container .brand-logo {*/
+    /*    height: 90px;*/
+    /*    width: auto;*/
+    /*}*/
 
     /* Mobile styles to ensure visibility and proper layout */
     @media (max-width: 767px) {
@@ -121,7 +121,7 @@
         <div class="navbar-header">
             <ul class="nav navbar-nav">
                 <li class="nav-item mobile-menu hidden-md-up float-xs-left"><a class="nav-link nav-menu-main menu-toggle hidden-xs"><i class="icon-menu5 font-large-1"></i></a></li>
-                <li class="nav-item logo-container"><a href="{{route('dashboard')}}" class="navbar-brand nav-link"><img alt="branding logo" src="{{ asset('dashboard/app-assets/images/logo.png') }}" data-expand="{{ asset('dashboard/app-assets/images/logo.png') }}" data-collapse="{{ asset('dashboard/app-assets/images/logo.png') }}" width="50%" class="brand-logo"></a></li>
+                <li class="nav-item logo-container"><a href="{{route('dashboard')}}" class="navbar-brand nav-link" style=""><img alt="branding logo" src="{{ asset('dashboard/app-assets/images/logo.png') }}" data-expand="{{ asset('dashboard/app-assets/images/logo.png') }}" data-collapse="{{ asset('dashboard/app-assets/images/logo.png') }}" width="30%" class="brand-logo"><span style="color: white">IBTKAR</span></a></li>
                 <li class="nav-item hidden-md-up float-xs-right"><a data-toggle="collapse" data-target="#navbar-mobile" class="nav-link open-navbar-container"><i class="icon-ellipsis pe-2x icon-icon-rotate-right-right"></i></a></li>
             </ul>
         </div>
