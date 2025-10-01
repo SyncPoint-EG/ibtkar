70c46505bde745c20bb266fd8b5bb8cbb5a81e2c
Yassin3007 <yassin.ahmed3070@gmail.com>
2025-10-02 01:26:51 +0300

    ..

--- resources/views/dashboard/students/show.blade.php
+++ resources/views/dashboard/students/show.blade.php
@@ -11,8 +11,10 @@
                 <div class="content-header-right breadcrumbs-right breadcrumbs-top col-md-6 col-xs-12">
                     <div class="breadcrumb-wrapper col-xs-12">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">{{ __('dashboard.common.dashboard') }}</a></li>
-                            <li class="breadcrumb-item"><a href="{{ route('students.index') }}">{{ __('dashboard.student.list') }}</a></li>
+                            <li class="breadcrumb-item"><a
+                                    href="{{ route('dashboard') }}">{{ __('dashboard.common.dashboard') }}</a></li>
+                            <li class="breadcrumb-item"><a
+                                    href="{{ route('students.index') }}">{{ __('dashboard.student.list') }}</a></li>
                             <li class="breadcrumb-item active">{{ __('dashboard.student.view') }}</li>
                         </ol>
                     </div>
@@ -29,8 +31,14 @@
                                     <a class="heading-elements-toggle"><i class="icon-ellipsis font-medium-3"></i></a>
                                     <div class="heading-elements">
                                         <ul class="list-inline mb-0">
-                                            <li><a href="{{ route('students.edit', $student->id) }}" class="btn btn-sm btn-primary"><i class="icon-pencil"></i> {{ __('dashboard.common.edit') }}</a></li>
-                                            <li><a href="{{ route('students.index') }}" class="btn btn-sm btn-secondary"><i class="icon-arrow-left4"></i> {{ __('dashboard.common.back') }}</a></li>
+                                            <li><a href="{{ route('students.edit', $student->id) }}"
+                                                   class="btn btn-sm btn-primary"><i
+                                                        class="icon-pencil"></i> {{ __('dashboard.common.edit') }}</a>
+                                            </li>
+                                            <li><a href="{{ route('students.index') }}"
+                                                   class="btn btn-sm btn-secondary"><i
+                                                        class="icon-arrow-left4"></i> {{ __('dashboard.common.back') }}
+                                                </a></li>
                                             <li><a data-action="collapse"><i class="icon-minus4"></i></a></li>
                                             <li><a data-action="expand"><i class="icon-expand2"></i></a></li>
                                         </ul>
@@ -49,44 +57,53 @@
                                                         </tr>
 
                                                         <div class="mb-3">
-        <strong>{{ __("dashboard.student.fields.first_name") }}:</strong> {{ $student->first_name }}
-    </div>
-                <div class="mb-3">
-        <strong>{{ __("dashboard.student.fields.last_name") }}:</strong> {{ $student->last_name }}
-    </div>
-                <div class="mb-3">
-        <strong>{{ __("dashboard.student.fields.phone") }}:</strong> {{ $student->phone }}
-    </div>
-                <div class="mb-3">
-        <strong>{{ __("dashboard.student.fields.password") }}:</strong> {{ $student->password }}
-    </div>
-                <div class="mb-3">
-        <strong>{{ __("dashboard.student.fields.governorate_id") }}:</strong> {{ $student->governorate_id }}
-    </div>
-                <div class="mb-3">
-        <strong>{{ __("dashboard.student.fields.district_id") }}:</strong> {{ $student->district_id }}
-    </div>
-                <div class="mb-3">
-        <strong>{{ __("dashboard.student.fields.center_id") }}:</strong> {{ $student->center_id }}
-    </div>
-                <div class="mb-3">
-        <strong>{{ __("dashboard.student.fields.stage_id") }}:</strong> {{ $student->stage_id }}
-    </div>
-                <div class="mb-3">
-        <strong>{{ __("dashboard.student.fields.grade_id") }}:</strong> {{ $student->grade_id }}
-    </div>
-                <div class="mb-3">
-        <strong>{{ __("dashboard.student.fields.division_id") }}:</strong> {{ $student->division_id }}
-    </div>
-                <div class="mb-3">
-        <strong>{{ __("dashboard.student.fields.gender") }}:</strong> {{ $student->gender }}
-    </div>
-                <div class="mb-3">
-        <strong>{{ __("dashboard.student.fields.birth_date") }}:</strong> {{ $student->birth_date }}
-    </div>
-                <div class="mb-3">
-        <strong>{{ __("dashboard.student.fields.status") }}:</strong> {{ $student->status }}
-    </div>
+                                                            <strong>{{ __("dashboard.student.fields.first_name") }}
+                                                                :</strong> {{ $student->first_name }}
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <strong>{{ __("dashboard.student.fields.last_name") }}
+                                                                :</strong> {{ $student->last_name }}
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <strong>{{ __("dashboard.student.fields.phone") }}
+                                                                :</strong> {{ $student->phone }}
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <strong>{{ __("dashboard.student.fields.governorate_id") }}
+                                                                :</strong> {{ $student->governorate_id }}
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <strong>{{ __("dashboard.student.fields.district_id") }}
+                                                                :</strong> {{ $student->district_id }}
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <strong>{{ __("dashboard.student.fields.center_id") }}
+                                                                :</strong> {{ $student->center_id }}
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <strong>{{ __("dashboard.student.fields.stage_id") }}
+                                                                :</strong> {{ $student->stage_id }}
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <strong>{{ __("dashboard.student.fields.grade_id") }}
+                                                                :</strong> {{ $student->grade_id }}
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <strong>{{ __("dashboard.student.fields.division_id") }}
+                                                                :</strong> {{ $student->division_id }}
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <strong>{{ __("dashboard.student.fields.gender") }}
+                                                                :</strong> {{ $student->gender }}
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <strong>{{ __("dashboard.student.fields.birth_date") }}
+                                                                :</strong> {{ $student->birth_date }}
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <strong>{{ __("dashboard.student.fields.status") }}
+                                                                :</strong> {{ $student->status }}
+                                                        </div>
 
                                                         <tr>
                                                             <th>{{ __('dashboard.common.created_at') }}</th>
@@ -106,7 +123,8 @@
                                 <div class="card-footer">
                                     <div class="row">
                                         <div class="col-md-12 text-right">
-                                            <form action="{{ route('students.destroy', $student->id) }}" method="POST" class="delete-form d-inline-block">
+                                            <form action="{{ route('students.destroy', $student->id) }}" method="POST"
+                                                  class="delete-form d-inline-block">
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="button" class="btn btn-danger btn-md delete-btn">
@@ -132,7 +150,10 @@
                                     <a class="heading-elements-toggle"><i class="icon-ellipsis font-medium-3"></i></a>
                                     <div class="heading-elements">
                                         <ul class="list-inline mb-0">
-                                            <li><a href="{{ route('students.export.lessons', $student->id) }}" class="btn btn-sm btn-success"><i class="icon-download"></i> {{ __('dashboard.common.export') }}</a></li>
+                                            <li><a href="{{ route('students.export.lessons', $student->id) }}"
+                                                   class="btn btn-sm btn-success"><i
+                                                        class="icon-download"></i> {{ __('dashboard.common.export') }}
+                                                </a></li>
                                             <li><a data-action="collapse"><i class="icon-minus4"></i></a></li>
                                             <li><a data-action="expand"><i class="icon-expand2"></i></a></li>
                                         </ul>
@@ -143,8 +164,12 @@
                                         <div class="row mb-2">
                                             <div class="col-md-12">
                                                 <form method="GET" action="{{ route('students.show', $student->id) }}">
-                                                    <input type="text" name="search" class="form-control" style="width: auto; display: inline-block;" placeholder="{{ __('dashboard.lesson.search_placeholder') }}" value="{{ request('search') }}">
-                                                    <button type="submit" class="btn btn-primary">{{ __('dashboard.common.search') }}</button>
+                                                    <input type="text" name="search" class="form-control"
+                                                           style="width: auto; display: inline-block;"
+                                                           placeholder="{{ __('dashboard.lesson.search_placeholder') }}"
+                                                           value="{{ request('search') }}">
+                                                    <button type="submit"
+                                                            class="btn btn-primary">{{ __('dashboard.common.search') }}</button>
                                                 </form>
                                             </div>
                                         </div>
@@ -168,7 +193,8 @@
                                                     </tr>
                                                 @empty
                                                     <tr>
-                                                        <td colspan="4" class="text-center">{{ __('dashboard.lesson.no_records') }}</td>
+                                                        <td colspan="4"
+                                                            class="text-center">{{ __('dashboard.lesson.no_records') }}</td>
                                                     </tr>
                                                 @endforelse
                                                 </tbody>
@@ -191,8 +217,8 @@
 
 @section('page_scripts')
     <script>
-        $(document).ready(function() {
-            $('.delete-btn').on('click', function(e) {
+        $(document).ready(function () {
+            $('.delete-btn').on('click', function (e) {
                 e.preventDefault();
 
                 // SweetAlert or custom confirmation
