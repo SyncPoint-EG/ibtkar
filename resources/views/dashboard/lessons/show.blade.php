48d1a059a84885a253c27a56784fb966baf56918
Yassin3007 <yassin.ahmed3070@gmail.com>
2025-10-14 16:50:37 +0300

    ..

--- resources/views/dashboard/lessons/show.blade.php
+++ resources/views/dashboard/lessons/show.blade.php
@@ -230,26 +230,36 @@
                                                                             method="POST" class="d-inline-block align-top mr-1">
                                                                             @csrf
                                                                             <div class="input-group">
-                                                                                <input type="number" name="watches"
-                                                                                       class="form-control"
-                                                                                       value="{{ $student->watches_count }}" style="width: 70px;">
-                                                                                <div class="input-group-append">
-                                                                                    <button type="submit"
-                                                                                            class="btn btn-sm btn-primary">{{ __('dashboard.common.update') }}</button>
+                                                                                <div class="row">
+                                                                                    <div class="col-lg-3"> <input type="number" name="watches"
+                                                                                                                  class="form-control"
+                                                                                                                  value="{{ $student->watches_count }}" style="width: 70px;"></div>
+                                                                                    <div class="col-lg-3">
+                                                                                        <button type="submit"
+                                                                                                class="btn btn-sm btn-primary">{{ __('dashboard.common.update') }}</button>
+                                                                                    </div>
                                                                                 </div>
+
+{{--                                                                                <div class="input-group-append">--}}
+{{--                                                                                   --}}
+{{--                                                                                </div>--}}
                                                                             </div>
                                                                         </form>
-                                                                        @if($payment)
-                                                                            <form
-                                                                                action="{{ route('payments.destroy', $payment->id) }}"
-                                                                                method="POST" class="delete-form d-inline-block align-top">
-                                                                                @csrf
-                                                                                @method('DELETE')
-                                                                                <button type="button" class="btn btn-sm btn-danger delete-payment-btn">
-                                                                                    <i class="icon-trash"></i> {{ __('dashboard.common.delete_payment') }}
-                                                                                </button>
-                                                                            </form>
-                                                                        @endif
+                                                                        <div class="col-lg-6">
+                                                                            @if($payment)
+                                                                                <form
+                                                                                    action="{{ route('payments.destroy', $payment->id) }}"
+                                                                                    method="POST" class="delete-form d-inline-block align-top">
+                                                                                    @csrf
+                                                                                    @method('DELETE')
+                                                                                    <button type="button" class="btn btn-sm btn-danger delete-payment-btn">
+                                                                                        <i class="icon-trash"></i> {{ __('dashboard.common.delete_payment') }}
+                                                                                    </button>
+                                                                                </form>
+                                                                            @endif
+                                                                        </div>
+
+
                                                                     </td>
                                                                 </tr>
                                                             @endforeach
