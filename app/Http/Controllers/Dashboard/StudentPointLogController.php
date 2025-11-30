896a4d2f7eea8a71b29f6921d8f6f6d2d61ccb4e
Yassin3007 <yassin.ahmed3070@gmail.com>
2025-11-30 02:19:26 +0200

    ..

--- app/Http/Controllers/Dashboard/StudentPointLogController.php
+++ app/Http/Controllers/Dashboard/StudentPointLogController.php
@@ -9,8 +9,10 @@ use Illuminate\Support\Facades\DB;
 
 class StudentPointLogController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $filters = $request->only(['name', 'phone', 'action_name']);
+
         $students = Student::query()
             ->withSum('pointLogs', 'points')
             ->withCount('pointLogs')
@@ -18,10 +20,29 @@ class StudentPointLogController extends Controller
                 $query->has('pointLogs')
                     ->orWhere('points', '>', 0);
             })
+            ->when($filters['name'] ?? null, function ($query, $name) {
+                $query->where(function ($q) use ($name) {
+                    $q->where('first_name', 'like', "%{$name}%")
+                        ->orWhere('last_name', 'like', "%{$name}%")
+                        ->orWhere(DB::raw("CONCAT(first_name, ' ', last_name)"), 'like', "%{$name}%");
+                });
+            })
+            ->when($filters['phone'] ?? null, function ($query, $phone) {
+                $query->where('phone', 'like', "%{$phone}%");
+            })
+            ->when($filters['action_name'] ?? null, function ($query, $action) {
+                $query->whereHas('pointLogs', function ($logQuery) use ($action) {
+                    $logQuery->where('action_name', 'like', "%{$action}%");
+                });
+            })
             ->orderByDesc(DB::raw('COALESCE(point_logs_sum_points, 0) + points'))
-            ->paginate(15);
+            ->paginate(15)
+            ->withQueryString();
 
-        return view('dashboard.points-logs.index', compact('students'));
+        return view('dashboard.points-logs.index', [
+            'students' => $students,
+            'filters' => $filters,
+        ]);
     }
 
     public function show(Student $student)
--- resources/views/dashboard/points-logs/index.blade.php
+++ resources/views/dashboard/points-logs/index.blade.php
@@ -20,6 +20,47 @@
             <div class="content-body">
                 <div class="card">
                     <div class="card-header">
+                        <h4 class="card-title">{{ __('dashboard.common.filters') }}</h4>
+                        <a class="heading-elements-toggle"><i class="icon-ellipsis font-medium-3"></i></a>
+                        <div class="heading-elements">
+                            <ul class="list-inline mb-0">
+                                <li><a data-action="collapse"><i class="icon-minus4"></i></a></li>
+                                <li><a data-action="expand"><i class="icon-expand2"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-body collapse in">
+                        <div class="card-block">
+                            <form action="{{ route('points-logs.index') }}" method="GET">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="name">{{ __('dashboard.student.fields.name') }}</label>
+                                            <input type="text" id="name" name="name" class="form-control" value="{{ $filters['name'] ?? '' }}" placeholder="Student name">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="phone">{{ __('dashboard.student.fields.phone') }}</label>
+                                            <input type="text" id="phone" name="phone" class="form-control" value="{{ $filters['phone'] ?? '' }}" placeholder="Phone">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="action_name">Action name</label>
+                                            <input type="text" id="action_name" name="action_name" class="form-control" value="{{ $filters['action_name'] ?? '' }}" placeholder="e.g. lesson_completed">
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary m-1">{{ __('dashboard.common.filter') }}</button>
+                                <a href="{{ route('points-logs.index') }}" class="btn btn-secondary m-1">{{ __('dashboard.common.reset') }}</a>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header">
                         <h4 class="card-title">Students with awarded points</h4>
                     </div>
                     <div class="card-body collapse in">
